17e8c0386635c129e3131f1082de5c1316b2d8eb
Shaun Mathias <smathias@companieshouse.gov.uk>
2018-07-23 16:11:38 +0100

    Removed Port reference from start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,6 @@ if [[ -z "${MESOS_SLAVE_PID}" ]]; then
     source ~/.chs_env/global_env
     source ~/.chs_env/document-generator/env
 
-    PORT="${DOCUMENT_GENERATION_SERVICE_PORT:=4017}"
 else
     PORT="$1"
     CONFIG_URL="$2"
