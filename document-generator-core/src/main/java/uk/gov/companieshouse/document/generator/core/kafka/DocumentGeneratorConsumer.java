a9c7ee45913c5f2ede6fc5bfdbb330852cf649c9
Shaun Mathias <smathias@companieshouse.gov.uk>
2018-08-10 16:01:19 +0100

    updated spaces within DocumentGeneratorConsumer

--- document-generator-core/src/main/java/uk/gov/companieshouse/document/generator/core/kafka/DocumentGeneratorConsumer.java
+++ document-generator-core/src/main/java/uk/gov/companieshouse/document/generator/core/kafka/DocumentGeneratorConsumer.java
@@ -13,19 +13,19 @@ import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
 public class DocumentGeneratorConsumer {
-    
-    private static final Logger LOG = LoggerFactory.getLogger("document-generator-core");
 
     private CHKafkaConsumerGroup consumerGroup;
 
+    private static final Logger LOG = LoggerFactory.getLogger("document-generator-core");
+
     /**
      * configure the DocumentGeneratorConsumerGroup to assign the broker address and connect
-     * 
+     *
      * @param consumerTopics
      * @param groupName
      */
     public DocumentGeneratorConsumer(List < String > consumerTopics, String groupName) {
-        
+
         ConsumerConfig config = constructConsumerConfig(consumerTopics, groupName);
         ConsumerConfigHelper.assignBrokerAddresses(config);
         generateLog(consumerTopics, groupName, config);
@@ -36,7 +36,7 @@ public class DocumentGeneratorConsumer {
 
     /**
      * poll the consumer group to consume messages from a List
-     * 
+     *
      * @return
      */
     public List < Message > pollConsumerGroup() {
@@ -59,46 +59,46 @@ public class DocumentGeneratorConsumer {
 
     /**
      * process the message again
-     * 
+     *
      * @param message
      */
     public void reprocess(Message message) {
         consumerGroup.reprocess(message);
     }
-    
+
     /**
      * generate log for structured logging
-     * 
+     *
      * @param consumerTopics
      * @param groupName
      * @param config
      */
     private void generateLog(List<String> consumerTopics, String groupName, ConsumerConfig config) {
-        
+
         Map < String, Object > logData = new HashMap < > ();
-        
+
         logData.put("kafka_broker_addresses", Arrays.toString(config.getBrokerAddresses()));
         logData.put("topic", consumerTopics);
         logData.put("group name", groupName);
-        
+
         LOG.trace("Connecting to Kafka consumer group", logData);
     }
-    
+
     /**
      * create the consumer configuration
-     * 
+     *
      * @param consumerTopics
      * @param groupName
      * @return
      */
     private ConsumerConfig constructConsumerConfig(List<String> consumerTopics, String groupName) {
-        
+
         ConsumerConfig config = new ConsumerConfig();
-        
+
         config.setTopics(consumerTopics);
         config.setGroupName(groupName);
         config.setResetOffset(false);
-        
+
         return config;
     }
 }
