30b3ef6473efd9bea52115fbb4058f826fe60cbf
ChrisJBurns <ChrisJBurns@users.noreply.github.com>
2018-09-12 12:46:46 +0100

    adds linktype enum to reduce duplication

--- document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/LinkType.java
+++ document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/LinkType.java
@@ -1,4 +1,17 @@
 package uk.gov.companieshouse.document.generator.accounts;
 
 public enum LinkType {
+    SELF("self"),
+    RESOURCE("resource");
+
+    private String link;
+
+    LinkType(String link) {
+        this.link = link;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
 }
