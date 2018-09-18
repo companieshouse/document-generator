6b67c357476eea8c6d73c5a5b96089f1543dbf85
ChrisJBurns <ChrisJBurns@users.noreply.github.com>
2018-09-18 13:15:29 +0100

    adds ServiceException accepting just a string

--- document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/exception/ServiceException.java
+++ document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/exception/ServiceException.java
@@ -2,6 +2,10 @@ package uk.gov.companieshouse.document.generator.accounts.exception;
 
 public class ServiceException extends Exception {
 
+    public ServiceException(String message) {
+        super(message);
+    }
+
     public ServiceException(String message, Throwable cause) {
         super(message, cause);
     }
