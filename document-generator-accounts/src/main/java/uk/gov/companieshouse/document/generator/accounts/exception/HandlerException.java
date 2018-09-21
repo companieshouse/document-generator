ba7560e446ba26a1d3358a8b78d57ab3ef24f251
ChrisJBurns <ChrisJBurns@users.noreply.github.com>
2018-09-21 12:30:21 +0100

    adds javadoc to exception classes

--- document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/exception/HandlerException.java
+++ document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/exception/HandlerException.java
@@ -1,11 +1,29 @@
 package uk.gov.companieshouse.document.generator.accounts.exception;
 
+/**
+ * The class {@code HandlerException} is a form of {@code Exception}
+ * that should be used at the handler layer to abstract lower level
+ * level exceptions from being propagated up the call stack.
+ */
 public class HandlerException extends Exception {
 
+    /**
+     * Constructs a new {@code HandlerException} with a custom message.
+     *
+     * @param message a custom message
+     */
     public HandlerException(String message) {
         super(message);
     }
 
+
+    /**
+     * Constructs a new {@code HandlerException} with a custom message and the specified
+     * cause.
+     *
+     * @param message a custom message
+     * @param cause the cause
+     */
     public HandlerException(String message, Throwable cause) {
         super(message, cause);
     }
--- document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/exception/ServiceException.java
+++ document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/exception/ServiceException.java
@@ -1,11 +1,28 @@
 package uk.gov.companieshouse.document.generator.accounts.exception;
 
+/**
+ * The class {@code ServiceException} is a form of {@code Exception}
+ * that should be used at the service layer to abstract lower level
+ * exceptions from being propagated up the call stack.
+ */
 public class ServiceException extends Exception {
 
+    /**
+     * Constructs a new {@code ServiceException} with a custom message.
+     *
+     * @param message a custom message
+     */
     public ServiceException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new {@code ServiceException} with a custom message and the specified
+     * cause.
+     *
+     * @param message a custom message
+     * @param cause the cause
+     */
     public ServiceException(String message, Throwable cause) {
         super(message, cause);
     }
