f4578fa608ef04f4893fd15fad5dbfc85cc24047
caismith-ch <csmith2@companieshouse.gov.uk>
2018-10-09 12:36:58 +0100

    Add transaction models and add spring data to pom

---
+++ document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/data/transaction/RejectReasons.java
@@ -0,0 +1,24 @@
+package uk.gov.companieshouse.document.generator.accounts.data.transaction;
+
+public class RejectReasons {
+
+    private String english;
+
+    private String welsh;
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+
+    public String getWelsh() {
+        return welsh;
+    }
+
+    public void setWelsh(String welsh) {
+        this.welsh = welsh;
+    }
+}
