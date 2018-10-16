77ae6ca5bead0ee77475023d172c85a243bacd4d
caismith-ch <csmith2@companieshouse.gov.uk>
2018-10-16 15:27:12 +0100

    Remove code that is now used by ApiClientService and implement ApiClientService to provide ApiClient

--- document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/data/accounts/AccountsManager.java
+++ document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/data/accounts/AccountsManager.java
@@ -1,20 +1,13 @@
 package uk.gov.companieshouse.document.generator.accounts.data.accounts;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import uk.gov.companieshouse.api.ApiClient;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
-import uk.gov.companieshouse.api.http.ApiKeyHttpClient;
-import uk.gov.companieshouse.api.http.HttpClient;
 import uk.gov.companieshouse.api.model.accounts.Accounts;
 import uk.gov.companieshouse.api.model.accounts.abridged.AbridgedAccountsApi;
-import uk.gov.companieshouse.environment.EnvironmentReader;
-import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.UUID;
+import uk.gov.companieshouse.document.generator.accounts.service.ApiClientService;
 
 /**
  * Temporary solution until private-sdk has been completed (SFA-518, SFA-670). When completed, this
@@ -24,11 +17,8 @@ import java.util.UUID;
 @Component
 public class AccountsManager {
 
-    private static final EnvironmentReader READER = new EnvironmentReaderImpl();
-
-    private final String apiUrl = READER.getMandatoryString("API_URL");
-    private final String chsApiKey = READER.getMandatoryString("CHS_API_KEY");
-    private static final String X_REQUEST_ID_HEADER = "x-request-id";
+    @Autowired
+    private ApiClientService apiClientService;
 
     /**
      * Get accounts resource if exists
@@ -40,18 +30,13 @@ public class AccountsManager {
      */
     public Accounts getAccounts(String link) throws ApiErrorResponseException, URIValidationException {
 
-        HttpClient httpClient = new ApiKeyHttpClient(chsApiKey);
-
-        setRequestId(httpClient);
-
-        ApiClient apiClient = new ApiClient(httpClient);
-        apiClient.setBasePath(apiUrl);
+        ApiClient apiClient = apiClientService.getApiClient();
 
         return apiClient.accounts().get(link).execute();
     }
 
     /**
-     *Get abridged accounts resource if exists
+     * Get abridged accounts resource if exists
      *
      * @param link - self link for the abridged accounts resource
      * @return AbridgedAccountsApi object
@@ -60,43 +45,8 @@ public class AccountsManager {
      */
     public AbridgedAccountsApi getAbridgedAccounts(String link) throws ApiErrorResponseException, URIValidationException {
 
-        HttpClient httpClient = new ApiKeyHttpClient(chsApiKey);
-
-        setRequestId(httpClient);
-
-        ApiClient apiClient = new ApiClient(httpClient);
-        apiClient.setBasePath(apiUrl);
+        ApiClient apiClient = apiClientService.getApiClient();
 
         return apiClient.abridgedAccounts().get(link).execute();
-    }
-
-    /**
-     *  Set request ID using httpclient
-     * @param httpClient
-     */
-    private static void setRequestId(HttpClient httpClient) {
-        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder
-                .currentRequestAttributes();
-        HttpServletRequest request = attr.getRequest();
-
-        String requestId = (String) request.getAttribute(X_REQUEST_ID_HEADER);
-
-        if (requestId == null)
-            requestId = request.getHeader(X_REQUEST_ID_HEADER);
-
-        if (requestId == null || requestId.isEmpty()) {
-            requestId = generateRequestId();
-            request.setAttribute(X_REQUEST_ID_HEADER, requestId);
-        }
-
-        httpClient.setRequestId(requestId);
-    }
-
-    /**
-     * Generate a universally unique identifier
-     * @return
-     */
-    private static String generateRequestId() {
-        return UUID.randomUUID().toString().substring(0,20);
     }
 }
