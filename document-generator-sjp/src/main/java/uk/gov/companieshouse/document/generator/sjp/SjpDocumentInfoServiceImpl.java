6641147542cb450c3f0f787941e03769de98171a
ch-tstrickland <tstrickland@companieshouse.gov.uk>
2019-02-26 15:35:19 +0000

    Bare minimum changes necessary to generate doc

--- document-generator-sjp/src/main/java/uk/gov/companieshouse/document/generator/sjp/SjpDocumentInfoServiceImpl.java
+++ document-generator-sjp/src/main/java/uk/gov/companieshouse/document/generator/sjp/SjpDocumentInfoServiceImpl.java
@@ -25,16 +25,10 @@ import uk.gov.companieshouse.logging.LoggerFactory;
 @Service
 public class SjpDocumentInfoServiceImpl implements DocumentInfoService {
 
-	private static final String PROSECTION_SERVICE = "http://chs-dev:41001";
+	private static final String PROSECTION_SERVICE = "http://chs-dev:4084";
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final EnvironmentReader READER = new EnvironmentReaderImpl();
 	
-//    @Autowired
-//    private TransactionService transactionService;
-//
-//    @Autowired
-//    private AccountsHandler accountsHandler;
-
     public static final String MODULE_NAME_SPACE = "document-generator-sjp";
 
     private static final Logger LOG = LoggerFactory.getLogger(MODULE_NAME_SPACE);
@@ -46,36 +40,11 @@ public class SjpDocumentInfoServiceImpl implements DocumentInfoService {
         String resourceId = documentInfoRequest.getResourceId();
         String resourceUri = documentInfoRequest.getResourceUri();
 
-//        Transaction transaction;
-//        try {
-//            transaction = transactionService.getTransaction(resourceId);
-//        } catch (ServiceException e) {
-//            LOG.error(e);
-//            return null;
-//        }
-
-//        String resourceLink =  Optional.of(transaction)
-//                .map(Transaction::getResources)
-//                .map(resources -> resources.get(resourceId))
-//                .map(Resource::getLinks)
-//                .map(links -> links.get(LinkType.RESOURCE.getLink()))
-//                .orElseGet(() -> {
-//                    LOG.info("Unable to find resource: " + resourceId + " in transaction: " + resourceUri);
-//                    return "";
-//                });
-
-//        JSONObject abridgedAccountJSON = new JSONObject(abridgedAccountData);
-//        JSONObject abridgedAccount = new JSONObject();
-//        abridgedAccount.put("abridged_account", abridgedAccountJSON);
-//        abridgedAccount.put("company_name", "company house");
-//        abridgedAccount.put("company_number", "1234");
-//        //return abridgedAccount.toString();
-
-		HttpHeaders requestHeaders = new HttpHeaders();
+        HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.set(AUTHORIZATION_HEADER, READER.getMandatoryString("CHS_API_KEY"));
-		
+
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-	    RestTemplate restTemplate = new RestTemplate(requestFactory);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
 
         HttpEntity<ProsecutionCase> requestEntity = new HttpEntity<>(requestHeaders);
 
@@ -87,58 +56,34 @@ public class SjpDocumentInfoServiceImpl implements DocumentInfoService {
 
         DocumentInfoResponse documentInfoResponse = new DocumentInfoResponse();
         documentInfoResponse.setData(getSjpData().toString());
-        documentInfoResponse.setAssetId("accounts");
+        documentInfoResponse.setAssetId("sjp");
         documentInfoResponse.setTemplateName("sjp.html");
         documentInfoResponse.setPath(String.format("/%s/%s", "sjp", "SJP-1234.pdf"));
         return documentInfoResponse;
+    }
 
-        // when the Accounts migration has been completed to Company Accounts, this code can be removed
-//        if (isAccounts(resourceLink)) {
-//            try {
-//                return accountsHandler.getAbridgedAccountsData(transaction, resourceLink);
-//            } catch (HandlerException e) {
-//                LOG.error(e);
-//            }
-//        }
+//    private ProsecutionCase callProsecutionService() {
+//        HttpHeaders requestHeaders = new HttpHeaders();
+//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+//        RestTemplate restTemplate = new RestTemplate(requestFactory);
 //
-//        return null;
-    }
+//        HttpEntity<Void> requestEntity = new HttpEntity<>(requestHeaders);
+//        ResponseEntity<ProsecutionCase> prosecutionCaseResponse = restTemplate.exchange(PROSECTION_SERVICE, HttpMethod.GET, requestEntity, ProsecutionCase.class);
+//
+//        return prosecutionCaseResponse.getBody();
+//    }
 
-	private ProsecutionCase callProsecutionService() {
-		
-		HttpHeaders requestHeaders = new HttpHeaders();
-		
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-	    RestTemplate restTemplate = new RestTemplate(requestFactory);
-
-        HttpEntity requestEntity = new HttpEntity(requestHeaders);
-
-        ResponseEntity<ProsecutionCase> prosecutionCaseResponse = restTemplate.exchange(PROSECTION_SERVICE, HttpMethod.GET, requestEntity, ProsecutionCase.class);
-
-        return prosecutionCaseResponse.getBody();
-	}
-
-    /**
-     * Determines if is an accounts specific link as "/transactions/{transactionId}/accounts/{accountsId}"
-     * only exists within the accounts (abridged) implementation
-     * @param resourceLink - resource link
-     * @return true if accounts, false if not
-     */
-    private boolean isAccounts(String resourceLink) {
-        return resourceLink.matches("/transactions\\/[0-9-]+/accsjounts\\/.*");
-    }
-    
     private JSONObject getSjpData() {
-		JSONObject pdfData = new JSONObject();
-		
-		pdfData.put("DefendantName", "Mr D Fraud");
-		pdfData.put("CompanyName", "companies house");
-		pdfData.put("CompanyNumber", "1234");
-		pdfData.put("CompanyAddress", "");
-		pdfData.put("URN", "URN123");
-		pdfData.put("DefendantDOB", "");
-		pdfData.put("template_registry_addr", "chs-dev:5002");
+        JSONObject pdfData = new JSONObject();
 
-		return pdfData;
+        pdfData.put("DefendantName", "Mr D Fraud");
+        pdfData.put("CompanyName", "companies house");
+        pdfData.put("CompanyNumber", "1234");
+        pdfData.put("CompanyAddress", "");
+        pdfData.put("URN", "URN123");
+        pdfData.put("DefendantDOB", "2000-01-01T15:04:05.999999999Z07:00");
+        pdfData.put("template_registry_addr", "chs-dev:5002");
+
+        return pdfData;
     }
 }
