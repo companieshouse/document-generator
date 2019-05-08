523cc55b345ce4e93714d505666948c5136af9ba
Shaun Mathias <smathias@companieshouse.gov.uk>
2019-05-08 12:46:34 +0100

    Updated template updated model

--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/model/CompanyReport.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/model/CompanyReport.java
@@ -1,9 +1,17 @@
 package uk.gov.companieshouse.document.generator.company.report.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 
+@JsonTypeName("company_report")
+@JsonTypeInfo(include = As.WRAPPER_OBJECT, use = Id.NAME)
 public class CompanyReport {
 
+    @JsonProperty("company_profile")
     private CompanyProfileApi companyProfileApi;
 
     public CompanyProfileApi getCompanyProfileApi() {
