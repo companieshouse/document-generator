b81e4380f1225751e57409f0766f9466f0fdc84a
rsachdevch <rsachdev@companieshouse.gov.uk>
2019-05-13 15:36:52 +0100

    Add offence transformer and unit tests
    Offence mapper have been added in order to transform offences coming
    in from the SDK to the model in DocGe.
    
    Resolves SJP-693

---
+++ document-generator-prosecution/src/main/java/uk/gov/companieshouse/document/generator/prosecution/mapping/model/offence/FilingPeriodType.java
@@ -0,0 +1,20 @@
+package uk.gov.companieshouse.document.generator.prosecution.mapping.model.offence;
+
+public enum FilingPeriodType {
+    /**
+     * Annual Return Period
+     */
+    ARP,
+    /**
+     * Annual Accounts Period
+     */
+    ACP,
+    /**
+     * Confirmation Statement Period
+     */
+    CSP,
+    /**
+     * All period types
+     */
+    ALL;
+}
