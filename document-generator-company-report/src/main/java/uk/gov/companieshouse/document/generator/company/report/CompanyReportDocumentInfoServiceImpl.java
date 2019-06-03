52b08779f490707e97be04ce19dfcbab5012c723
caismith-ch <csmith2@companieshouse.gov.uk>
2019-06-03 14:41:07 +0100

    Update parent pom so that common dependicies are only added once and add company report to bean configuration

--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/CompanyReportDocumentInfoServiceImpl.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/CompanyReportDocumentInfoServiceImpl.java
@@ -1,4 +1,14 @@
 package uk.gov.companieshouse.document.generator.company.report;
 
-public class CompanyReportDocumentInfoServiceImpl {
+import uk.gov.companieshouse.document.generator.interfaces.DocumentInfoService;
+import uk.gov.companieshouse.document.generator.interfaces.exception.DocumentInfoException;
+import uk.gov.companieshouse.document.generator.interfaces.model.DocumentInfoRequest;
+import uk.gov.companieshouse.document.generator.interfaces.model.DocumentInfoResponse;
+
+public class CompanyReportDocumentInfoServiceImpl implements DocumentInfoService {
+
+    @Override
+    public DocumentInfoResponse getDocumentInfo(DocumentInfoRequest documentInfoRequest) throws DocumentInfoException {
+        return null;
+    }
 }
