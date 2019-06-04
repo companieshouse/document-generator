245af959e215f3bc8f345f9e1119645422e7ad52
leahtaylor <ltaylor@companieshouse.gov.uk>
2019-06-04 13:46:34 +0100

    Add relevent models for cover page of company report

---
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/mapping/model/documentmodels/CompanyReport.java
@@ -0,0 +1,23 @@
+package uk.gov.companieshouse.document.generator.company.report.mapping.model.documentmodels;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import uk.gov.companieshouse.document.generator.company.report.mapping.model.documentmodels.items.registrationinformation.RegistrationInformation;
+
+@JsonInclude(Include.NON_NULL)
+@JsonTypeName("company_report")
+public class CompanyReport {
+
+    @JsonProperty("company_registration_information")
+    private RegistrationInformation registrationInformation;
+
+    public RegistrationInformation getRegistrationInformation() {
+        return registrationInformation;
+    }
+
+    public void setRegistrationInformation(RegistrationInformation registrationInformation) {
+        this.registrationInformation = registrationInformation;
+    }
+}
