c1ff76452c1c9e01aad41a15622b1bc15b1732a2
rsachdevch <rsachdev@companieshouse.gov.uk>
2019-06-11 09:54:41 +0100

    Remove unused import

--- document-generator-prosecution/src/main/java/uk/gov/companieshouse/document/generator/prosecution/service/ProsecutionService.java
+++ document-generator-prosecution/src/main/java/uk/gov/companieshouse/document/generator/prosecution/service/ProsecutionService.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriTemplate;
 
 import uk.gov.companieshouse.api.InternalApiClient;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
