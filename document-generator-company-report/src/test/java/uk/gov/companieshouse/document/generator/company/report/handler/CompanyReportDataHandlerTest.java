b07157fc13fcc822cca07f75dbac728e14f21d06
caismith-ch <csmith2@companieshouse.gov.uk>
2019-06-11 08:18:04 +0100

    Remove unused import

--- document-generator-company-report/src/test/java/uk/gov/companieshouse/document/generator/company/report/handler/CompanyReportDataHandlerTest.java
+++ document-generator-company-report/src/test/java/uk/gov/companieshouse/document/generator/company/report/handler/CompanyReportDataHandlerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import sun.jvm.hotspot.utilities.Assert;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.document.generator.company.report.mapping.mappers.CompanyReportMapper;
 import uk.gov.companieshouse.document.generator.company.report.mapping.model.CompanyReportApiData;
