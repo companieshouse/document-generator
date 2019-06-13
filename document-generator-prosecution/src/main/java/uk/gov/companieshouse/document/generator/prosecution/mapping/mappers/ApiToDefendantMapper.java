8815ebbf24d809b08519350665b5b6ba73183b91
rsachdevch <rsachdev@companieshouse.gov.uk>
2019-06-13 13:32:18 +0100

    Refactor processing of string for defendant name

--- document-generator-prosecution/src/main/java/uk/gov/companieshouse/document/generator/prosecution/mapping/mappers/ApiToDefendantMapper.java
+++ document-generator-prosecution/src/main/java/uk/gov/companieshouse/document/generator/prosecution/mapping/mappers/ApiToDefendantMapper.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.document.generator.prosecution.mapping.mappers;
 
+import org.apache.commons.lang.StringUtils;
 import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -8,6 +9,9 @@ import org.mapstruct.Mappings;
 import org.springframework.web.context.annotation.RequestScope;
 import uk.gov.companieshouse.api.model.prosecution.defendant.DefendantApi;
 import uk.gov.companieshouse.document.generator.prosecution.mapping.model.defendant.Defendant;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RequestScope
 @Mapper(componentModel = "spring")
@@ -18,25 +22,17 @@ public interface ApiToDefendantMapper {
         if(defendantApi.getIsCorporateAppointment()) {
             defendant.setName(defendantApi.getCompanyOfficerDetailsApi().getCompanyName());
         } else {
-            StringBuilder name = new StringBuilder();
             String title = defendantApi.getPersonOfficerDetailsApi().getTitle();
             String forename = defendantApi.getPersonOfficerDetailsApi().getForename();
             String middleName = defendantApi.getPersonOfficerDetailsApi().getMiddleName();
             String surname = defendantApi.getPersonOfficerDetailsApi().getSurname();
 
-            if(title != null && title.trim().length() > 0) {
-                name.append(title).append(" ");
-            }
-            if(forename != null && forename.trim().length() > 0) {
-                name.append(forename).append(" ");
-            }
-            if(middleName != null && middleName.trim().length() > 0){
-                name.append(middleName).append(" ");
-            }
-            if(surname != null && surname.trim().length() > 0) {
-                name.append(surname.trim());
-            }
-            defendant.setName(name.toString().trim());
+            String name = Stream.of(title, forename, middleName, surname)
+                    .filter(StringUtils::isNotBlank)
+                    .map(String::trim)
+                    .collect(Collectors.joining(" "));
+
+            defendant.setName(name.trim());
         }
     }
 
