4f9dbf64f3bd7314ea7d3a3294d866521902e06a
bsaunders95 <bsaunders@companieshouse.gov.uk>
2019-06-18 16:33:44 +0100

    Add supporting mapper unit tests

--- document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/mapping/cic/mappers/ApiToApprovalMapper.java
+++ document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/mapping/cic/mappers/ApiToApprovalMapper.java
@@ -1,10 +1,14 @@
 package uk.gov.companieshouse.document.generator.accounts.mapping.cic.mappers;
 
+import java.time.LocalDate;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import org.springframework.web.context.annotation.RequestScope;
+import uk.gov.companieshouse.accountsdates.AccountsDatesHelper;
+import uk.gov.companieshouse.accountsdates.impl.AccountsDatesHelperImpl;
 import uk.gov.companieshouse.api.model.accounts.cic.approval.CicApprovalApi;
 import uk.gov.companieshouse.document.generator.accounts.mapping.cic.model.Approval;
 
@@ -12,13 +16,21 @@ import uk.gov.companieshouse.document.generator.accounts.mapping.cic.model.Appro
 @Mapper(componentModel = "spring")
 public interface ApiToApprovalMapper {
 
+    AccountsDatesHelper accountsDatesHelper = new AccountsDatesHelperImpl();
+
     ApiToApprovalMapper INSTANCE = Mappers.getMapper(ApiToApprovalMapper.class);
 
     @Mappings({
             @Mapping(source = "name",
                     target = "name"),
             @Mapping(source = "date",
-                    target = "date"),
+                    target = "date",
+                    qualifiedByName = "localDateToDisplayDateMapper"),
     })
     Approval apiToApproval(CicApprovalApi cicApproval);
+
+    @Named("localDateToDisplayDateMapper")
+    default String localDateToDisplayDate(LocalDate date) {
+        return accountsDatesHelper.convertLocalDateToDisplayDate(date);
+    }
 }
