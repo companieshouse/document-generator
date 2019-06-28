533f46c9619bd931d5dce74d1465105a914a91c6
leahtaylor <ltaylor@companieshouse.gov.uk>
2019-06-28 11:30:16 +0100

    Make test variables private static final

--- document-generator-company-report/src/test/java/uk/gov/companieshouse/document/generator/company/report/mapping/mappers/ApiToRegistrationInformationMapperTest.java
+++ document-generator-company-report/src/test/java/uk/gov/companieshouse/document/generator/company/report/mapping/mappers/ApiToRegistrationInformationMapperTest.java
@@ -34,24 +34,24 @@ public class ApiToRegistrationInformationMapperTest {
     @Mock
     private RetrieveApiEnumerationDescription mockRetrieveApiEnumerations;
 
-    private String COMPANY_NUMBER = "00004598";
-    private String COMPANY_NAME = "Test Company LTD";
-    private String COMPANY_STATUS = "active-proposal-to-strike-off";
-    private String COMPANY_STATUS_DETAILS = "community-interest-company";
-    private String COMPANY_TYPE = "private-unlimited";
-    private String COMPANY_SUB_TYPE = "community-interest-company";
-    private LocalDate DATE_OF_CREATION = LocalDate.of(2019, 06, 06);
-    private String DATE_OF_CREATION_FORMATTED = "6 June 2019";
+    private static final String COMPANY_NUMBER = "00004598";
+    private static final String COMPANY_NAME = "Test Company LTD";
+    private static final String COMPANY_STATUS = "active-proposal-to-strike-off";
+    private static final String COMPANY_STATUS_DETAILS = "community-interest-company";
+    private static final String COMPANY_TYPE = "private-unlimited";
+    private static final String COMPANY_SUB_TYPE = "community-interest-company";
+    private static final LocalDate DATE_OF_CREATION = LocalDate.of(2019, 06, 06);
+    private static final String DATE_OF_CREATION_FORMATTED = "6 June 2019";
 
-    private String ADDRESS_LINE_ONE = "address line 1";
-    private String ADDRESS_LINE_TWO = "address line 2";
-    private String CARE_OF = "care of";
-    private String COUNTRY = "country";
-    private String LOCALITY = "locality";
-    private String PO_BOX = "po box";
-    private String POSTAL_CODE = "postal code";
-    private String REGION = "region";
-    private String PREMISE = "premise";
+    private static final String ADDRESS_LINE_ONE = "address line 1";
+    private static final String ADDRESS_LINE_TWO = "address line 2";
+    private static final String CARE_OF = "care of";
+    private static final String COUNTRY = "country";
+    private static final String LOCALITY = "locality";
+    private static final String PO_BOX = "po box";
+    private static final String POSTAL_CODE = "postal code";
+    private static final String REGION = "region";
+    private static final String PREMISE = "premise";
 
     private String[] SIC_CODES = new String[]{"5231", "5232", "5233"};
 
