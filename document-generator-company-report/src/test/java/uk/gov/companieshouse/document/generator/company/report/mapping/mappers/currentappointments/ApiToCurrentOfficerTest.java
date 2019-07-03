df575e4191e522e9aa1aab45b11743c792986a6b
caismith-ch <csmith2@companieshouse.gov.uk>
2019-07-03 10:49:25 +0100

    Increase coverage of officer mapper test

--- document-generator-company-report/src/test/java/uk/gov/companieshouse/document/generator/company/report/mapping/mappers/currentappointments/ApiToCurrentOfficerTest.java
+++ document-generator-company-report/src/test/java/uk/gov/companieshouse/document/generator/company/report/mapping/mappers/currentappointments/ApiToCurrentOfficerTest.java
@@ -11,6 +11,7 @@ import uk.gov.companieshouse.api.model.common.DateOfBirth;
 import uk.gov.companieshouse.api.model.officers.CompanyOfficerApi;
 import uk.gov.companieshouse.api.model.officers.FormerNamesApi;
 import uk.gov.companieshouse.api.model.officers.IdentificationApi;
+import uk.gov.companieshouse.api.model.officers.OfficerRoleApi;
 import uk.gov.companieshouse.document.generator.company.report.mapping.model.document.items.currentappointments.items.CurrentOfficer;
 
 import java.io.IOException;
@@ -18,6 +19,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith({MockitoExtension.class})
@@ -30,9 +32,8 @@ public class ApiToCurrentOfficerTest {
     private static final String NAME = "name";
     private static final LocalDate APPOINTED_ON = LocalDate.of(
         2019, 06, 06);
+
     private static final String COUNTRY_OF_RESIDENCE = "country of residence";
-
-
     private static final String ADDRESS_LINE_ONE = "address line 1";
     private static final String ADDRESS_LINE_TWO = "address line 2";
     private static final String CARE_OF = "care of";
@@ -42,6 +43,15 @@ public class ApiToCurrentOfficerTest {
     private static final String POSTAL_CODE = "postal code";
     private static final String REGION = "region";
     private static final String PREMISE = "premise";
+
+    private static final Long MONTH = 1L;
+    private static final Long YEAR = 1993L;
+
+    private static final String IDENTIFICATION_TYPE = "identification type";
+    private static final String LEGAL_AUTHORITY = "legal authority";
+    private static final String LEGAL_FORM = "legal form";
+    private static final String PLACE_REGISTRATION = "place registration";
+    private static final String REGISTRATION_NUMBER = "registration number";
 
     @Test
     @DisplayName("tests company profile data maps to registration information model")
@@ -53,7 +63,20 @@ public class ApiToCurrentOfficerTest {
             apiToCurrentOfficer.apiToCurrentOfficer(companyOfficerApi);
 
         assertNotNull(currentOfficer);
-        
+        assertEquals(NAME, currentOfficer.getName());
+        assertEquals(ADDRESS_LINE_ONE, currentOfficer.getAddress().getAddressLine1());
+        assertEquals(ADDRESS_LINE_TWO, currentOfficer.getAddress().getAddressLine2());
+        assertEquals(CARE_OF, currentOfficer.getAddress().getCareOf());
+        assertEquals(COUNTRY, currentOfficer.getAddress().getCountry());
+        assertEquals(LOCALITY, currentOfficer.getAddress().getLocality());
+        assertEquals(PO_BOX, currentOfficer.getAddress().getPoBox());
+        assertEquals(POSTAL_CODE, currentOfficer.getAddress().getPostalCode());
+        assertEquals(REGION, currentOfficer.getAddress().getRegion());
+        assertEquals(PREMISE, currentOfficer.getAddress().getPremises());
+        assertEquals(MONTH, currentOfficer.getDateOfBirth().getMonth());
+        assertEquals(YEAR, currentOfficer.getDateOfBirth().getYear());
+        assertEquals(COUNTRY_OF_RESIDENCE, currentOfficer.getCountryOfResidence());
+        assertEquals(APPOINTED_ON, currentOfficer.getAppointed());
     }
 
     private CompanyOfficerApi createCompanyOfficerApi() {
@@ -64,7 +87,6 @@ public class ApiToCurrentOfficerTest {
         companyOfficerApi.setDateOfBirth(createDateOfBirth());
         companyOfficerApi.setCountryOfResidence(COUNTRY_OF_RESIDENCE);
         companyOfficerApi.setAppointedOn(APPOINTED_ON);
-        companyOfficerApi.setFormerNames(createFormerNames());
         companyOfficerApi.setIdentification(createIdentification());
 
         return companyOfficerApi;
@@ -88,38 +110,20 @@ public class ApiToCurrentOfficerTest {
 
     private DateOfBirth createDateOfBirth() {
         DateOfBirth dateOfBirth = new DateOfBirth();
-        dateOfBirth.setMonth(1L);
-        dateOfBirth.setYear(1993L);
+        dateOfBirth.setMonth(MONTH);
+        dateOfBirth.setYear(YEAR);
 
         return dateOfBirth;
-    }
-
-    private List<FormerNamesApi> createFormerNames() {
-        List<FormerNamesApi> formerNames = new ArrayList<>();
-
-        FormerNamesApi formerName1 = new FormerNamesApi();
-        FormerNamesApi formerName2 = new FormerNamesApi();
-
-        formerName1.setForenames("forename1");
-        formerName2.setForenames("forename2");
-
-        formerName1.setSurname("surname1");
-        formerName2.setSurname("surname2");
-
-        formerNames.add(formerName1);
-        formerNames.add(formerName2);
-
-        return formerNames;
     }
 
     private IdentificationApi createIdentification() {
         IdentificationApi identificationApi = new IdentificationApi();
 
-        identificationApi.setIdentificationType("identification type");
-        identificationApi.setLegalAuthority("legal authority");
-        identificationApi.setLegalForm("legal form");
-        identificationApi.setPlaceRegistration("place registration");
-        identificationApi.setRegistrationNumber("registration number");
+        identificationApi.setIdentificationType(IDENTIFICATION_TYPE);
+        identificationApi.setLegalAuthority(LEGAL_AUTHORITY);
+        identificationApi.setLegalForm(LEGAL_FORM);
+        identificationApi.setPlaceRegistration(PLACE_REGISTRATION);
+        identificationApi.setRegistrationNumber(REGISTRATION_NUMBER);
 
         return identificationApi;
     }
