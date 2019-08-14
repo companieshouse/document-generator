15107de32d74665a8799dc6a188f6010e1f9397c
caismith-ch <csmith2@companieshouse.gov.uk>
2019-08-14 16:41:16 +0100

    Add charges service to access the chages data using the sdk

---
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/service/ChargesService.java
@@ -0,0 +1,40 @@
+package uk.gov.companieshouse.document.generator.company.report.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriTemplate;
+import uk.gov.companieshouse.api.ApiClient;
+import uk.gov.companieshouse.api.error.ApiErrorResponseException;
+import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.api.model.charges.ChargesApi;
+import uk.gov.companieshouse.document.generator.company.report.exception.ServiceException;
+
+@Service
+public class ChargesService {
+
+    @Autowired
+    CompanyReportApiClientService companyReportApiClientService;
+
+    private static final UriTemplate GET_CHARGES_URI =
+        new UriTemplate("/company/{companyNumber}/charges");
+
+    public ChargesApi getCharges(String companyNumber) throws ServiceException {
+
+        ChargesApi chargesApi;
+
+        ApiClient apiClient = companyReportApiClientService.getApiClient();
+
+        String uri = GET_CHARGES_URI.expand(companyNumber).toString();
+
+        try {
+            chargesApi = apiClient.charges().get(uri).execute().getData();
+        } catch (ApiErrorResponseException e) {
+
+            throw new ServiceException("Error retrieving company charges", e);
+        } catch (URIValidationException e) {
+
+            throw new ServiceException("Invalid URI for company charges", e);
+        }
+
+        return chargesApi;
+    }
+}
