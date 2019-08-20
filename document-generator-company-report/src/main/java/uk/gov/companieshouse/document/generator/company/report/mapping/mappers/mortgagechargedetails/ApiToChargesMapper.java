efc8d4d7f3a51cdef41a1f8836bb494ca917152c
caismith-ch <csmith2@companieshouse.gov.uk>
2019-08-20 17:50:20 +0100

    Remove variables that will not be used and add particulars type and details

--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/mapping/mappers/mortgagechargedetails/ApiToChargesMapper.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/mapping/mappers/mortgagechargedetails/ApiToChargesMapper.java
@@ -1,31 +1,44 @@
 package uk.gov.companieshouse.document.generator.company.report.mapping.mappers.mortgagechargedetails;
 
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 import org.springframework.web.context.annotation.RequestScope;
 import uk.gov.companieshouse.api.model.charges.ChargeApi;
 import uk.gov.companieshouse.document.generator.company.report.mapping.model.document.items.mortgagechargedetails.items.Charge;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RequestScope
-@Mapper(componentModel = "spring", uses = {ApiToPersonsEntitledMapper.class})
+@Mapper(componentModel = "spring")
 public abstract class ApiToChargesMapper {
 
+    private static final String D_MMMM_UUUU = "d MMMM uuuu";
+
     @Mappings({
-        @Mapping(source = "acquiredOn", target = "acquisitionDate"),
-        @Mapping(source = "assetsCeasedReleased", target = "assetsCeased"),
-        @Mapping(source = "securedDetails.description", target = "securedDetailsDescription"),
+        @Mapping(source = "classification.description", target = "description"),
+        @Mapping(source = "createdOn", target = "createdDate"),
+        @Mapping(source = "deliveredOn", target = "delivered"),
         @Mapping(source = "particulars.type", target = "type"),
         @Mapping(source = "particulars.description", target = "particularsDescription"),
-        @Mapping(source = "particulars.chargorActingAsBareTrustee", target = "chargorActingAsBareTrustee"),
-        @Mapping(source = "particulars.containsFixedCharge", target = "containsFixedCharge"),
-        @Mapping(source = "particulars.containsFloatingCharge", target = "containsFloatingCharge"),
-        @Mapping(source = "particulars.containsNegativeCharge", target = "containsNegativePledge"),
-        @Mapping(source = "particulars.floatingChargeCoversAll", target = "floatingChargeCoversAll"),
     })
     public abstract Charge apiToCharge(ChargeApi chargeApi);
 
     public abstract List<Charge> apiToCharge(List<ChargeApi> chargeApi);
+
+    @AfterMapping
+    protected void convertDate(ChargeApi chargeApi, @MappingTarget Charge charge) {
+
+        if(chargeApi != null) {
+            charge.setCreatedDate(chargeApi.getCreatedOn().format(DateTimeFormatter.ofPattern(D_MMMM_UUUU)));
+            charge.setDelivered(chargeApi.getDeliveredOn().format(DateTimeFormatter.ofPattern(D_MMMM_UUUU)));
+
+            if (chargeApi.getSatisfiedOn() != null) {
+                charge.setSatisfiedOn(chargeApi.getSatisfiedOn().format(DateTimeFormatter.ofPattern(D_MMMM_UUUU)));
+            }
+        }
+    }
 }
--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/mapping/model/document/items/mortgagechargedetails/items/Charge.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/mapping/model/document/items/mortgagechargedetails/items/Charge.java
@@ -12,62 +12,23 @@ public class Charge {
     @JsonProperty("description")
     private String description;
 
-    @JsonProperty("charge_code")
-    private String chargeCode;
-
-    @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonProperty("created_date")
-    private LocalDate createdDate;
+    private String createdDate;
 
-    @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonProperty("delivered")
-    private LocalDate delivered;
+    private String delivered;
 
     @JsonProperty("status")
     private String status;
 
-    @JsonProperty("transaction")
-    private String transaction;
-
-    @JsonDeserialize(using = LocalDateDeserializer.class)
-    @JsonProperty("acquisition_date")
-    private LocalDate acquisitionDate;
-
-    @JsonProperty("assets_ceased")
-    private String assetsCeased;
-
-    @JsonProperty("persons_entitled")
-    private List<PersonEntitled> personsEntitled;
-
-    @JsonProperty("more_than_four_persons_entitled")
-    private Boolean moreThanFourPersonsEntitled;
-
-    @JsonProperty("secured_details_description")
-    private String securedDetailsDescription;
-
-    @JsonProperty("secured_details_type")
-    private String securedDetailsType;
+    @JsonProperty("satisfied_on")
+    private String satisfiedOn;
 
     @JsonProperty("particulars_type")
     private String type;
 
     @JsonProperty("particulars_description")
     private String particularsDescription;
-
-    @JsonProperty("chargor_acting_as_bare_trustee")
-    private Boolean chargorActingAsBareTrustee;
-
-    @JsonProperty("contains_fixed_charge")
-    private Boolean containsFixedCharge;
-
-    @JsonProperty("contains_floating_charge")
-    private Boolean containsFloatingCharge;
-
-    @JsonProperty("contains_negative_pledge")
-    private Boolean containsNegativePledge;
-
-    @JsonProperty("floating_charge_covers_all")
-    private Boolean floatingChargeCoversAll;
 
     public String getDescription() {
         return description;
@@ -77,27 +38,19 @@ public class Charge {
         this.description = description;
     }
 
-    public String getChargeCode() {
-        return chargeCode;
-    }
-
-    public void setChargeCode(String chargeCode) {
-        this.chargeCode = chargeCode;
-    }
-
-    public LocalDate getCreatedDate() {
+    public String getCreatedDate() {
         return createdDate;
     }
 
-    public void setCreatedDate(LocalDate createdDate) {
+    public void setCreatedDate(String createdDate) {
         this.createdDate = createdDate;
     }
 
-    public LocalDate getDelivered() {
+    public String getDelivered() {
         return delivered;
     }
 
-    public void setDelivered(LocalDate delivered) {
+    public void setDelivered(String delivered) {
         this.delivered = delivered;
     }
 
@@ -109,60 +62,12 @@ public class Charge {
         this.status = status;
     }
 
-    public String getTransaction() {
-        return transaction;
+    public String getSatisfiedOn() {
+        return satisfiedOn;
     }
 
-    public void setTransaction(String transaction) {
-        this.transaction = transaction;
-    }
-
-    public LocalDate getAcquisitionDate() {
-        return acquisitionDate;
-    }
-
-    public void setAcquisitionDate(LocalDate acquisitionDate) {
-        this.acquisitionDate = acquisitionDate;
-    }
-
-    public String getAssetsCeased() {
-        return assetsCeased;
-    }
-
-    public void setAssetsCeased(String assetsCeased) {
-        this.assetsCeased = assetsCeased;
-    }
-
-    public List<PersonEntitled> getPersonsEntitled() {
-        return personsEntitled;
-    }
-
-    public void setPersonsEntitled(List<PersonEntitled> personsEntitled) {
-        this.personsEntitled = personsEntitled;
-    }
-
-    public Boolean getMoreThanFourPersonsEntitled() {
-        return moreThanFourPersonsEntitled;
-    }
-
-    public void setMoreThanFourPersonsEntitled(Boolean moreThanFourPersonsEntitled) {
-        this.moreThanFourPersonsEntitled = moreThanFourPersonsEntitled;
-    }
-
-    public String getSecuredDetailsDescription() {
-        return securedDetailsDescription;
-    }
-
-    public void setSecuredDetailsDescription(String securedDetailsDescription) {
-        this.securedDetailsDescription = securedDetailsDescription;
-    }
-
-    public String getSecuredDetailsType() {
-        return securedDetailsType;
-    }
-
-    public void setSecuredDetailsType(String securedDetailsType) {
-        this.securedDetailsType = securedDetailsType;
+    public void setSatisfiedOn(String satisfiedOn) {
+        this.satisfiedOn = satisfiedOn;
     }
 
     public String getType() {
@@ -179,45 +84,5 @@ public class Charge {
 
     public void setParticularsDescription(String particularsDescription) {
         this.particularsDescription = particularsDescription;
-    }
-
-    public Boolean getChargorActingAsBareTrustee() {
-        return chargorActingAsBareTrustee;
-    }
-
-    public void setChargorActingAsBareTrustee(Boolean chargorActingAsBareTrustee) {
-        this.chargorActingAsBareTrustee = chargorActingAsBareTrustee;
-    }
-
-    public Boolean getContainsFixedCharge() {
-        return containsFixedCharge;
-    }
-
-    public void setContainsFixedCharge(Boolean containsFixedCharge) {
-        this.containsFixedCharge = containsFixedCharge;
-    }
-
-    public Boolean getContainsFloatingCharge() {
-        return containsFloatingCharge;
-    }
-
-    public void setContainsFloatingCharge(Boolean containsFloatingCharge) {
-        this.containsFloatingCharge = containsFloatingCharge;
-    }
-
-    public Boolean getContainsNegativePledge() {
-        return containsNegativePledge;
-    }
-
-    public void setContainsNegativePledge(Boolean containsNegativePledge) {
-        this.containsNegativePledge = containsNegativePledge;
-    }
-
-    public Boolean getFloatingChargeCoversAll() {
-        return floatingChargeCoversAll;
-    }
-
-    public void setFloatingChargeCoversAll(Boolean floatingChargeCoversAll) {
-        this.floatingChargeCoversAll = floatingChargeCoversAll;
     }
 }
