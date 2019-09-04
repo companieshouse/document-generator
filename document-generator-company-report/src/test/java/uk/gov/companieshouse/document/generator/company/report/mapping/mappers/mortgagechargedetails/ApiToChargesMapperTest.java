6acda924ede6d8c63a21813d349b87a37e4d6375
Shaun Mathias <smathias@companieshouse.gov.uk>
2019-09-04 16:08:38 +0100

    Updated intial tests Updated template

--- document-generator-company-report/src/test/java/uk/gov/companieshouse/document/generator/company/report/mapping/mappers/mortgagechargedetails/ApiToChargesMapperTest.java
+++ document-generator-company-report/src/test/java/uk/gov/companieshouse/document/generator/company/report/mapping/mappers/mortgagechargedetails/ApiToChargesMapperTest.java
@@ -1,6 +1,5 @@
 package uk.gov.companieshouse.document.generator.company.report.mapping.mappers.mortgagechargedetails;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -12,17 +11,8 @@ import uk.gov.companieshouse.api.model.charges.ChargeApi;
 import uk.gov.companieshouse.api.model.charges.ClassificationApi;
 import uk.gov.companieshouse.api.model.charges.ParticularsApi;
 import uk.gov.companieshouse.document.generator.common.descriptions.RetrieveApiEnumerationDescription;
-import uk.gov.companieshouse.document.generator.company.report.mapping.model.document.items.mortgagechargedetails.items.Charge;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -44,35 +34,15 @@ public class ApiToChargesMapperTest {
     private static final LocalDate SATISFIED_ON = LocalDate.of(2018,03,03);
     private static final String SATISFIED_ON_CONVERTED = "3 March 2018";
 
-    @BeforeEach
-    void init() {
-        when(mockRetrieveApiEnumerationDescription.getApiEnumerationDescription(anyString(), anyString(),
-            anyString(), any())).thenReturn(STATUS);
-    }
 
     @Test
-    @DisplayName("tests multiple Charge data maps to Charge model")
-    void testMultipleChargesMaps() {
+    @DisplayName("tests Charge data maps to Charge model")
+    void testChargeMaps() {
+
+
 
     }
 
-    @Test
-    @DisplayName("tests single Charge data maps to Charge model")
-    void testSingleChargeMaps() {
-
-    }
-
-    private List<ChargeApi> createChargeList() {
-        List<ChargeApi> chargeApiList = new ArrayList<>();
-
-        ChargeApi chargeApi1 = createChargeApi();
-        ChargeApi chargeApi2 = createChargeApi();
-
-        chargeApiList.add(chargeApi1);
-        chargeApiList.add(chargeApi2);
-
-        return chargeApiList;
-    }
 
     private ChargeApi createChargeApi() {
         ChargeApi chargeApi = new ChargeApi();
--- document-generator-company-report/src/test/java/uk/gov/companieshouse/document/generator/company/report/mapping/mappers/mortgagechargedetails/ApiToMortgageChargeDetailsMapperTest.java
+++ document-generator-company-report/src/test/java/uk/gov/companieshouse/document/generator/company/report/mapping/mappers/mortgagechargedetails/ApiToMortgageChargeDetailsMapperTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.api.model.charges.ChargeApi;
 import uk.gov.companieshouse.api.model.charges.ChargesApi;
 import uk.gov.companieshouse.document.generator.company.report.mapping.model.document.items.mortgagechargedetails.MortgageChargeDetails;
 import uk.gov.companieshouse.document.generator.company.report.mapping.model.document.items.mortgagechargedetails.items.Charge;
@@ -16,11 +17,13 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class ApiToMortgageChargeDetaislMapperTest {
+public class ApiToMortgageChargeDetailsMapperTest {
 
     @Mock
     private ApiToChargesMapper mockApiToChargesMapper;
@@ -32,42 +35,25 @@ public class ApiToMortgageChargeDetaislMapperTest {
     @DisplayName("Test charges api data maps to charges model")
     void testApiToChargesMaps() {
 
-        ChargesApi chargesApi = createChargesApi();
+        when(mockApiToChargesMapper.apiToCharge(anyList())).thenReturn(new ArrayList<>());
 
-        List<Charge> chargeList = createChargeList();
-
-        when(mockApiToChargesMapper.apiToCharge(chargesApi.getItems())).thenReturn(chargeList);
-
-        MortgageChargeDetails mortgageChargeDetails = apiToMortgageChargeDetailsMapper.apiToMortgageChargeDetails(chargesApi);
+        MortgageChargeDetails mortgageChargeDetails = apiToMortgageChargeDetailsMapper.apiToMortgageChargeDetails(createChargesApi());
 
         assertNotNull(mortgageChargeDetails);
-        assertEquals(1L, mortgageChargeDetails.getTotalCount().longValue());
+        assertEquals(5L, mortgageChargeDetails.getTotalCount().longValue());
         assertEquals(1L, mortgageChargeDetails.getSatisfiedCount().longValue());
-        assertEquals(1L, mortgageChargeDetails.getPartSatisfiedCount().longValue());
+        assertEquals(2L, mortgageChargeDetails.getPartSatisfiedCount().longValue());
+        assertEquals(2L, mortgageChargeDetails.getOutstanding().longValue());
     }
 
     private ChargesApi createChargesApi() {
 
         ChargesApi chargesApi = new ChargesApi();
-
-        chargesApi.setTotalCount(1L);
+        chargesApi.setPartSatisfiedCount(2L);
         chargesApi.setSatisfiedCount(1L);
-        chargesApi.setPartSatisfiedCount(1L);
+        chargesApi.setTotalCount(5L);
+        chargesApi.setItems(new ArrayList<>());
 
         return chargesApi;
-    }
-
-    private List<Charge> createChargeList() {
-        List<Charge> charges = new ArrayList<>();
-        Charge charge1 = new Charge();
-        charge1.setStatus("status1");
-
-        Charge charge2 = new Charge();
-        charge2.setStatus("status2");
-
-        charges.add(charge1);
-        charges.add(charge2);
-
-        return charges;
     }
 }
