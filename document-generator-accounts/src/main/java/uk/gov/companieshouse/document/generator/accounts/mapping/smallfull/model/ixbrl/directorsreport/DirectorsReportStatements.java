251296d40e15276c9b08843a4afa99ce08693a6b
Faisal Amin <famin@companieshouse.gov.uk>
2020-01-23 20:11:48 +0000

    Revert version of pom.xml

--- document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/mapping/smallfull/model/ixbrl/directorsreport/DirectorsReportStatements.java
+++ document-generator-accounts/src/main/java/uk/gov/companieshouse/document/generator/accounts/mapping/smallfull/model/ixbrl/directorsreport/DirectorsReportStatements.java
@@ -2,8 +2,12 @@ package uk.gov.companieshouse.document.generator.accounts.mapping.smallfull.mode
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.google.gson.Gson;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+import java.util.Objects;
+
+@JsonInclude(Include.NON_NULL)
 public class DirectorsReportStatements {
 
     @JsonProperty("additional_information")
@@ -48,5 +52,27 @@ public class DirectorsReportStatements {
 
     public void setPrincipalActivities(String principalActivities) {
         this.principalActivities = principalActivities;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DirectorsReportStatements that = (DirectorsReportStatements) o;
+        return Objects.equals(additionalInformation, that.additionalInformation) &&
+                Objects.equals(companyPolicyOnDisabledEmployees, that.companyPolicyOnDisabledEmployees) &&
+                Objects.equals(politicalAndCharitableDonations, that.politicalAndCharitableDonations) &&
+                Objects.equals(principalActivities, that.principalActivities);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(additionalInformation, companyPolicyOnDisabledEmployees, politicalAndCharitableDonations, principalActivities);
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
     }
 }
