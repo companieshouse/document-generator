b649472fbe9698dc053289239e8485ad621c8cc7
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2021-08-23 12:15:58 +0100

    Amendments to try to circumvent the full company profile transformation

--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/handler/DissolvedCompanyReportDataHandler.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/handler/DissolvedCompanyReportDataHandler.java
@@ -18,8 +18,10 @@ import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.api.model.filinghistory.FilingHistoryApi;
 import uk.gov.companieshouse.document.generator.company.report.exception.HandlerException;
 import uk.gov.companieshouse.document.generator.company.report.mapping.mappers.CompanyReportMapper;
+import uk.gov.companieshouse.document.generator.company.report.mapping.mappers.recentfilinghistory.ApiToRecentFilingHistoryMapper;
 import uk.gov.companieshouse.document.generator.company.report.mapping.model.CompanyReportApiData;
 import uk.gov.companieshouse.document.generator.company.report.mapping.model.document.CompanyReport;
+import uk.gov.companieshouse.document.generator.company.report.mapping.model.document.items.registrationinformation.RegistrationInformation;
 import uk.gov.companieshouse.document.generator.company.report.service.oracle.FilingHistoryServiceOracle;
 import uk.gov.companieshouse.document.generator.interfaces.model.DocumentInfoResponse;
 import uk.gov.companieshouse.logging.Logger;
@@ -28,99 +30,114 @@ import uk.gov.companieshouse.logging.LoggerFactory;
 @Component
 public class DissolvedCompanyReportDataHandler {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MODULE_NAME_SPACE);
+	private static final Logger LOG = LoggerFactory.getLogger(MODULE_NAME_SPACE);
 
-    private static final String COMPANY_REPORT = "company-report";
+	private static final String COMPANY_REPORT = "company-report";
 
-    @Autowired
-    private FilingHistoryServiceOracle filingHistoryServiceOracle;
+	@Autowired
+	private FilingHistoryServiceOracle filingHistoryServiceOracle;
 
-    private CompanyReportMapper companyReportMapper;
+	//private CompanyReportMapper companyReportMapper;
 
-    public DocumentInfoResponse getCompanyReport(String companyNumber, String requestId) throws HandlerException {
+	@Autowired
+	private ApiToRecentFilingHistoryMapper apiToRecentFilingHistoryMapper;
 
-        ZonedDateTime timeStamp = ZonedDateTime.now();
-        Map<String, Object> debugMap = getDebugMap(companyNumber);
-        debugMap.put("request_id", requestId);
+	public DocumentInfoResponse getCompanyReport(String companyNumber, String requestId) throws HandlerException {
 
-        LOG.info("Getting data for dissolved company report", debugMap);
-        return createDocumentInfoResponse(companyNumber, requestId, timeStamp);
-    }
+		ZonedDateTime timeStamp = ZonedDateTime.now();
+		Map<String, Object> debugMap = getDebugMap(companyNumber);
+		debugMap.put("request_id", requestId);
 
-    private DocumentInfoResponse createDocumentInfoResponse(String companyNumber, String requestId,
-            ZonedDateTime timeStamp) throws HandlerException {
+		LOG.info("Getting data for dissolved company report", debugMap);
+		return createDocumentInfoResponse(companyNumber, requestId, timeStamp);
+	}
 
-        DocumentInfoResponse documentInfoResponse = new DocumentInfoResponse();
+	private DocumentInfoResponse createDocumentInfoResponse(String companyNumber, String requestId,
+			ZonedDateTime timeStamp) throws HandlerException {
 
-        documentInfoResponse.setData(getCompanyReportData(companyNumber, requestId, timeStamp));
-        documentInfoResponse.setAssetId(COMPANY_REPORT);
-        documentInfoResponse.setPath(createPathString());
-        documentInfoResponse.setTemplateName("company-report.html");
-        documentInfoResponse.setDescriptionIdentifier(COMPANY_REPORT);
+		DocumentInfoResponse documentInfoResponse = new DocumentInfoResponse();
 
-        return documentInfoResponse;
-    }
+		documentInfoResponse.setData(getCompanyReportData(companyNumber, requestId, timeStamp));
+		documentInfoResponse.setAssetId(COMPANY_REPORT);
+		documentInfoResponse.setPath(createPathString());
+		documentInfoResponse.setTemplateName("company-report.html");
+		documentInfoResponse.setDescriptionIdentifier(COMPANY_REPORT);
 
-    private String getCompanyReportData(String companyNumber, String requestId, ZonedDateTime timeStamp)
-            throws HandlerException {
-        CompanyReportApiData companyReportApiData = new CompanyReportApiData();
+		return documentInfoResponse;
+	}
 
-        CompanyProfileApi companyProfileApi = new CompanyProfileApi();
-        companyProfileApi.setCompanyNumber(companyNumber);
+	private String getCompanyReportData(String companyNumber, String requestId, ZonedDateTime timeStamp)
+			throws HandlerException {
+		CompanyReportApiData companyReportApiData = new CompanyReportApiData();
 
-        setCompanyReportData(companyNumber, requestId, companyReportApiData, companyProfileApi);
-        LOG.info("Company num : " + companyNumber + " requestId : " + requestId + "companyProfileApi" + companyProfileApi.toString());
-        LOG.info("Filing History : " + companyReportApiData.getFilingHistoryApi().toString());
-        LOG.info("Timestamp : " + timeStamp);
+		CompanyProfileApi companyProfileApi = new CompanyProfileApi();
+		companyProfileApi.setCompanyNumber(companyNumber);
 
-        return toJson(companyReportMapper.mapCompanyReport(companyReportApiData, requestId, companyNumber),
-                companyNumber, requestId, timeStamp);
-    }
+		setCompanyReportData(companyNumber, requestId, companyReportApiData, companyProfileApi);
+		LOG.info("Company num : " + companyNumber + " requestId : " + requestId + "companyProfileApi"
+				+ companyProfileApi.toString());
+		LOG.info("Filing History : " + companyReportApiData.getFilingHistoryApi().toString());
+		LOG.info("Timestamp : " + timeStamp);
 
-    private void setCompanyReportData(String companyNumber, String requestId, CompanyReportApiData companyReportApiData,
-            CompanyProfileApi companyProfileApi) {
-        setFilingHistoryData(companyNumber, requestId, companyReportApiData, companyProfileApi);
-    }
+		return toJson(mapCompanyReport(companyProfileApi, companyReportApiData),
+				companyNumber, requestId, timeStamp);
+	}
 
-    private void setFilingHistoryData(String companyNumber, String requestId, CompanyReportApiData companyReportApiData,
-            CompanyProfileApi companyProfileApi) {
-        FilingHistoryApi filingHistoryApi = filingHistoryServiceOracle.getFilingHistory(companyNumber);
-        companyReportApiData.setFilingHistoryApi(filingHistoryApi);
-    }
+	private CompanyReport mapCompanyReport(CompanyProfileApi companyProfileApi,
+			CompanyReportApiData companyReportApiData) {
+		CompanyReport companyReport = new CompanyReport();
+		RegistrationInformation registrationInformation = new RegistrationInformation();
+		registrationInformation.setCompanyNumber(companyProfileApi.getCompanyNumber());
+		companyReport.setRegistrationInformation(registrationInformation);
+		companyReport.setRecentFilingHistory(apiToRecentFilingHistoryMapper
+				.apiToRecentFilingHistoryMapper(companyReportApiData.getFilingHistoryApi().getItems()));
+		return companyReport;
+	}
 
-    private String createPathString() {
-        return String.format("/%s/%s", COMPANY_REPORT, getUniqueFileName());
-    }
+	private void setCompanyReportData(String companyNumber, String requestId, CompanyReportApiData companyReportApiData,
+			CompanyProfileApi companyProfileApi) {
+		setFilingHistoryData(companyNumber, requestId, companyReportApiData, companyProfileApi);
+	}
 
-    private String getUniqueFileName() {
-        UUID uuid = UUID.randomUUID();
-        return "companyReport" + uuid.toString() + ".html";
-    }
+	private void setFilingHistoryData(String companyNumber, String requestId, CompanyReportApiData companyReportApiData,
+			CompanyProfileApi companyProfileApi) {
+		FilingHistoryApi filingHistoryApi = filingHistoryServiceOracle.getFilingHistory(companyNumber);
+		companyReportApiData.setFilingHistoryApi(filingHistoryApi);
+	}
 
-    private Map<String, Object> getDebugMap(String companyNumber) {
-        Map<String, Object> debugMap = new HashMap<>();
-        debugMap.put("company_number", companyNumber);
-        return debugMap;
-    }
+	private String createPathString() {
+		return String.format("/%s/%s", COMPANY_REPORT, getUniqueFileName());
+	}
 
-    private String toJson(CompanyReport companyReport, String companyNumber, String requestId, ZonedDateTime timeStamp)
-            throws HandlerException {
+	private String getUniqueFileName() {
+		UUID uuid = UUID.randomUUID();
+		return "companyReport" + uuid.toString() + ".html";
+	}
 
-        String reportToJson;
-        ObjectMapper mapper = new ObjectMapper();
+	private Map<String, Object> getDebugMap(String companyNumber) {
+		Map<String, Object> debugMap = new HashMap<>();
+		debugMap.put("company_number", companyNumber);
+		return debugMap;
+	}
 
-        companyReport.setTimeStampCreated(timeStamp.format(DateTimeFormatter.ofPattern("d MMMM uuuu HH:mm:ss")));
+	private String toJson(CompanyReport companyReport, String companyNumber, String requestId, ZonedDateTime timeStamp)
+			throws HandlerException {
 
-        try {
-            LOG.infoContext(requestId, "Attempting to convert company report to JSON", getDebugMap(companyNumber));
-            reportToJson = mapper.writeValueAsString(companyReport);
-        } catch (JsonProcessingException e) {
-            throw new HandlerException(new StringBuilder(
-                    "Could not serialise Document data for the generation of the company report for company: ")
-                            .append(companyReport.getRegistrationInformation().getCompanyName()).append("-")
-                            .append(companyReport.getRegistrationInformation().getCompanyNumber()).toString());
-        }
+		String reportToJson;
+		ObjectMapper mapper = new ObjectMapper();
 
-        return reportToJson;
-    }
+		companyReport.setTimeStampCreated(timeStamp.format(DateTimeFormatter.ofPattern("d MMMM uuuu HH:mm:ss")));
+
+		try {
+			LOG.infoContext(requestId, "Attempting to convert company report to JSON", getDebugMap(companyNumber));
+			reportToJson = mapper.writeValueAsString(companyReport);
+		} catch (JsonProcessingException e) {
+			throw new HandlerException(new StringBuilder(
+					"Could not serialise Document data for the generation of the company report for company: ")
+							.append(companyReport.getRegistrationInformation().getCompanyName()).append("-")
+							.append(companyReport.getRegistrationInformation().getCompanyNumber()).toString());
+		}
+
+		return reportToJson;
+	}
 }
