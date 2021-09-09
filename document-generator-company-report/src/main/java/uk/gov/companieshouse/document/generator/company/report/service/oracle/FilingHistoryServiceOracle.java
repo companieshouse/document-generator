63feb81c6ccd5271a65d489192aee4b7fb5d2cef
caismith-ch <csmith2@companieshouse.gov.uk>
2021-09-09 15:22:26 +0100

    Fix FilingHistoryServiceOracleTest

--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/service/oracle/FilingHistoryServiceOracle.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/service/oracle/FilingHistoryServiceOracle.java
@@ -1,33 +1,33 @@
 package uk.gov.companieshouse.document.generator.company.report.service.oracle;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplate;
-
 import uk.gov.companieshouse.api.model.filinghistory.FilingHistoryApi;
+import uk.gov.companieshouse.environment.EnvironmentReader;
 
 @Service
 public class FilingHistoryServiceOracle {
-    
-    private static String FILING_HISTORY_URL = "/company/{companyNumber}/filing-history";
+
+    private static final String FILING_HISTORY_URL = "/company/{companyNumber}/filing-history";
+    private static final String ORACLE_QUERY_API_URL_ENV_VARIABLE = "ORACLE_QUERY_API_URL";
     private static final UriTemplate FILING_HISTORY_URI = new UriTemplate(FILING_HISTORY_URL);
     
-    @Value("${ORACLE_QUERY_API_URL}")
-    private String oracleQueryApiUrl;
-    
     private RestTemplate restTemplate;
-    
+    private EnvironmentReader environmentReader;
+
     @Autowired
-    public FilingHistoryServiceOracle(final RestTemplate restTemplate) {
+    public FilingHistoryServiceOracle(final RestTemplate restTemplate,
+                                      final EnvironmentReader environmentReader) {
         this.restTemplate = restTemplate;
+        this.environmentReader = environmentReader;
     }
     
     public FilingHistoryApi getFilingHistory(String companyNumber) {
+        String baseUrl = environmentReader.getMandatoryString(ORACLE_QUERY_API_URL_ENV_VARIABLE);
         String url = FILING_HISTORY_URI.expand(companyNumber).toString();
-        FilingHistoryApi response = restTemplate.getForObject(oracleQueryApiUrl + url, FilingHistoryApi.class);
-        
-        return response;
+
+        return restTemplate.getForObject(baseUrl + url, FilingHistoryApi.class);
     }
 }
--- document-generator-company-report/src/test/java/Service/FilingHistoryServiceOracleTest.java
+++ document-generator-company-report/src/test/java/Service/FilingHistoryServiceOracleTest.java
@@ -1,9 +1,13 @@
 package Service;
 
 
-import jdk.nashorn.internal.objects.annotations.Setter;
-import org.junit.Rule;
-import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -14,17 +18,10 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriTemplate;
 import uk.gov.companieshouse.api.model.filinghistory.FilingHistoryApi;
 import uk.gov.companieshouse.document.generator.company.report.service.oracle.FilingHistoryServiceOracle;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import uk.gov.companieshouse.environment.EnvironmentReader;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -33,29 +30,32 @@ public class FilingHistoryServiceOracleTest {
     @Mock
     private RestTemplate restTemplateMock;
 
+    @Mock
+    private EnvironmentReader mockEnvironmentReader;
+
     @InjectMocks
     private FilingHistoryServiceOracle filingHistoryServiceOracle;
 
     @Captor
     ArgumentCaptor<String> captor;
 
-    @Rule
-    public final EnvironmentVariables environmentVariables
-            = new EnvironmentVariables();
+    private static final String COMPANY_NUMBER = "00000000";
+    private static final String ORACLE_QUERY_API_URL_ENV_VARIABLE = "ORACLE_QUERY_API_URL";
+
 
     @Test
     @DisplayName("Test filing history api response is not null")
     void testFilingHistoryServiceOracle(){
-        environmentVariables.set("ORACLE_QUERY_API_URL", "oracle/query/api/url");
-
-        String companyNumber = "00006400";
-
         FilingHistoryApi filingHistoryApi = new FilingHistoryApi();
+
+        doReturn("oracle/query/api/url").when(mockEnvironmentReader).getMandatoryString(anyString());
+
         when(restTemplateMock.getForObject(captor.capture(), ArgumentMatchers.eq(FilingHistoryApi.class))).thenReturn(filingHistoryApi);
-        FilingHistoryApi response = filingHistoryServiceOracle.getFilingHistory(companyNumber);
+
+        FilingHistoryApi response = filingHistoryServiceOracle.getFilingHistory(COMPANY_NUMBER);
 
         verify(restTemplateMock).getForObject(System.getenv("ORACLE_QUERY_API_URL") + captor.capture(), ArgumentMatchers.eq(FilingHistoryApi.class));
-        assertEquals("oracle/query/api/url/company/00006400/filing-history", captor.getValue());
+        assertEquals("oracle/query/api/url/company/00000000/filing-history", captor.getValue());
         assertNotNull(response);
 
     }
