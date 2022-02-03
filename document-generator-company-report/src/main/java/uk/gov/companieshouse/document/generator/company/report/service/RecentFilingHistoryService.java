099b67918b7f79b310ba498d0168fdd7723596c7
caismith-ch <csmith2@companieshouse.gov.uk>
2022-02-03 12:40:47 +0000

    BI-10395 - Implement ability to page through results to return full company filing history

--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/service/RecentFilingHistoryService.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/service/RecentFilingHistoryService.java
@@ -15,6 +15,10 @@ public class RecentFilingHistoryService {
 
     private CompanyReportApiClientService companyReportApiClientService;
 
+    private static final String ITEMS_PER_PAGE_KEY = "items_per_page";
+    private static final String ITEMS_PER_PAGE_VALUE = "100";
+    private static final String START_INDEX_KEY = "start_index";
+
     @Autowired
     public RecentFilingHistoryService(CompanyReportApiClientService companyReportApiClientService) {
         this.companyReportApiClientService = companyReportApiClientService;
@@ -25,23 +29,43 @@ public class RecentFilingHistoryService {
 
     public FilingHistoryApi getFilingHistory(String companyNumber) throws ServiceException {
 
-        FilingHistoryApi filingHistoryApi;
+        FilingHistoryApi filingHistoryApi = null;
 
         ApiClient apiClient = companyReportApiClientService.getApiClient();
 
+        int startIndex = 0;
+        int itemsPerPage = 100;
+
+        filingHistoryApi = retrieveFilingHistory(companyNumber, apiClient, startIndex, itemsPerPage);
+
+        while (filingHistoryApi.getItems().size() < filingHistoryApi.getTotalCount()) {
+            startIndex += itemsPerPage;
+            FilingHistoryApi moreResults = retrieveFilingHistory(companyNumber, apiClient, startIndex, itemsPerPage);
+            filingHistoryApi.getItems().addAll(moreResults.getItems());
+        }
+
+        return filingHistoryApi;
+    }
+
+    private FilingHistoryApi retrieveFilingHistory(String companyNumber, ApiClient apiClient, Integer startIndex, Integer itemsPerPage)
+        throws ServiceException {
+
         String uri = GET_RECENT_FILING_HISTORY_URI.expand(companyNumber).toString();
+
+        FilingHistoryApi filingHistoryApi;
 
         try {
             FilingHistoryList filingHistoryList = apiClient.filingHistory().list(uri);
-            filingHistoryList.addQueryParams("items_per_page", "100");
+            filingHistoryList.addQueryParams(ITEMS_PER_PAGE_KEY, ITEMS_PER_PAGE_VALUE);
+            filingHistoryList.addQueryParams(START_INDEX_KEY, startIndex.toString());
 
             filingHistoryApi = filingHistoryList.execute().getData();
         } catch (ApiErrorResponseException e) {
 
-            throw new ServiceException("Error retrieving recent filing history", e);
+            throw new ServiceException("Error retrieving filing history items", e);
         } catch (URIValidationException e) {
 
-            throw new ServiceException("Invalid URI for recent filing history resource", e);
+            throw new ServiceException("Invalid URI for filing history resource", e);
         }
         return filingHistoryApi;
     }
