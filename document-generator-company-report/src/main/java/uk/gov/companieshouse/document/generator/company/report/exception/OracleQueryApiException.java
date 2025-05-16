478e65916773242c9d687113b50815198ff05bb2
Steve Hicks <shicks@companieshouse.gov.uk>
2025-05-16 17:21:41 +0100

    BI-14430:add new ApiClientService and update CompanyServiceOracle for using API rather than Spring Rest. Add new runtime exception when calling the api for oracle-query-api to mimic what happens in Spring RestTemplate

---
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/exception/OracleQueryApiException.java
@@ -0,0 +1,14 @@
+package uk.gov.companieshouse.document.generator.company.report.exception;
+
+/**
+ * OracleQueryApiException is a custom exception that is thrown when there is an
+ * error while calling the `oracle-query-api` service.
+ *
+ * This is a runtime exception since the previous code that used Spring RestClient also through a runtime exception
+ */
+public class OracleQueryApiException extends RuntimeException {
+
+    public OracleQueryApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/service/ApiClientService.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.document.generator.company.report.service;
+
+import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.api.InternalApiClient;
+import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
+
+@Component
+public class ApiClientService {
+
+    public InternalApiClient getInternalApiClient() {
+        return ApiSdkManager.getPrivateSDK();
+    }
+}
