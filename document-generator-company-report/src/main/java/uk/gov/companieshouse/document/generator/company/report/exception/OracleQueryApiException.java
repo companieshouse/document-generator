cb7410bc9164a721c3a6c1e86f308c9f7574f4a1
Steve Hicks <shicks@companieshouse.gov.uk>
2025-05-20 09:15:34 +0100

    BI-14430: corrected spelling mistake

--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/exception/OracleQueryApiException.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/exception/OracleQueryApiException.java
@@ -4,7 +4,7 @@ package uk.gov.companieshouse.document.generator.company.report.exception;
  * OracleQueryApiException is a custom exception that is thrown when there is an
  * error while calling the `oracle-query-api` service.
  *
- * This is a runtime exception since the previous code that used Spring RestClient also through a runtime exception
+ * This is a runtime exception since the previous code that used Spring RestClient also threw a runtime exception
  */
 public class OracleQueryApiException extends RuntimeException {
 
