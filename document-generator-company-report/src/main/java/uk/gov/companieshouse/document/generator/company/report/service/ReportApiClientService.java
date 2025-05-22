1ab387eed641c7430f3eac20d48b0cfee066244b
Steve Hicks <shicks@companieshouse.gov.uk>
2025-05-22 18:03:56 +0100

    BI-14430 - Working in CHS Docker but test class needs change on CHS Docker and also mapping for company profile is not yet working

--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/service/ReportApiClientService.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/service/ReportApiClientService.java
@@ -1,17 +1,27 @@
 package uk.gov.companieshouse.document.generator.company.report.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.InternalApiClient;
 import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
 
 /**
  * Service to get the internal API client.
+ * Need to set the Base Path to use the oracleQueryApiUrl.
  * Use "Report" as a prefix since this module is imported into other projects that already have an ApiClientService.
  */
 @Component
 public class ReportApiClientService {
 
+    private final String oracleQueryApiUrl;
+
+    public ReportApiClientService(@Value("${ORACLE_QUERY_API_URL}") String oracleQueryApiUrl) {
+        this.oracleQueryApiUrl = oracleQueryApiUrl;
+    }
+
     public InternalApiClient getInternalApiClient() {
-        return ApiSdkManager.getPrivateSDK();
+        InternalApiClient internalApiClient = ApiSdkManager.getInternalSDK();
+        internalApiClient.setBasePath(oracleQueryApiUrl);
+        return internalApiClient;
     }
 }
--- document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/service/oracle/CompanyServiceOracle.java
+++ document-generator-company-report/src/main/java/uk/gov/companieshouse/document/generator/company/report/service/oracle/CompanyServiceOracle.java
@@ -3,12 +3,15 @@ package uk.gov.companieshouse.document.generator.company.report.service.oracle;
 import static uk.gov.companieshouse.document.generator.company.report.CompanyReportDocumentInfoServiceImpl.MODULE_NAME_SPACE;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.document.generator.company.report.exception.OracleQueryApiException;
 import uk.gov.companieshouse.document.generator.company.report.service.ReportApiClientService;
+import uk.gov.companieshouse.document.generator.company.report.service.oracle.mapper.CompanyProfileMapper;
+import uk.gov.companieshouse.document.generator.company.report.service.oracle.mapper.TempMapper;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 import uk.gov.companieshouse.logging.util.DataMap;
@@ -16,15 +19,21 @@ import uk.gov.companieshouse.logging.util.DataMap;
 @Service
 public class CompanyServiceOracle {
 
+    @Value("${ORACLE_QUERY_API_URL}")
+    private String oracleQueryApiUrl;
+
     public static final String COMPANY = "/company/";
 
     private static final Logger LOG = LoggerFactory.getLogger(MODULE_NAME_SPACE);
 
     private final ReportApiClientService apiClientService;
+    // private final CompanyProfileMapper companyProfileMapper;
+    private final TempMapper companyProfileMapper = new TempMapper();
 
     @Autowired
-    public CompanyServiceOracle(ReportApiClientService apiClientService) {
+    public CompanyServiceOracle(ReportApiClientService apiClientService/*, CompanyProfileMapper companyProfileMapper*/) {
         this.apiClientService = apiClientService;
+        //this.companyProfileMapper = companyProfileMapper;
     }
 
     public CompanyProfileApi getCompanyProfile(String companyNumber) throws OracleQueryApiException {
@@ -33,13 +42,19 @@ public class CompanyServiceOracle {
                 build();
         LOG.info("CompanyServiceOracle: Retrieving Company Profile", requestDataMap.getLogMap());
 
+        var internalApiClient = apiClientService.getInternalApiClient();
+
+        LOG.debug("Base URL used [" + internalApiClient.getBasePath() + "]");
+
         try {
-            return apiClientService
-                    .getInternalApiClient()
-                    .company()
-                    .get(COMPANY + companyNumber)
+            var companyProfileData = internalApiClient
+                    .privateCompanyResourceHandler()
+                    .getCompanyFullProfile(COMPANY + companyNumber)
                     .execute()
                     .getData();
+
+            return companyProfileMapper.toCompanyProfileApi(companyProfileData);
+
         } catch (URIValidationException | ApiErrorResponseException e) {
             var message = String.format("Error Retrieving Company Profile data for %s", companyNumber);
             LOG.error(message, e, requestDataMap.getLogMap());
